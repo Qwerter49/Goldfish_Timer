b5e5c2d90c8d910ccc74515d5f2086c2ddd12c58
Jay <jaykamas@gmail.com>
2020-09-08 18:01:37 -0400

    Added Javascript to userShow page.

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,11 @@
-fetch("http://localhost:3000/user_timers")
+const userSection = document.querySelector("section")
+fetch('http://localhost:3000/users')
     .then(response => response.json())
-    .then(timers => console.log(timers))
+    .then(users => {
+        users.forEach(user => {
+            console.log(user)
+            const userName = document.createElement('h2')
+            userName.innerHTML = `<a href="userShow.html?id=${user.id}">${user.username}</a>`
+            userSection.append(userName)
+        })
+    })
