7d25fb883ab108d4c4f86e5ec4f4e5db8e5ae1d1
Jay <jaykamas@gmail.com>
2020-09-09 16:18:39 -0400

    Timer working

--- frontend/timerShow.js
+++ frontend/timerShow.js
@@ -6,17 +6,31 @@ fetch(`http://localhost:3000/timers/${id}`)
     .then(timer => {
         const name = document.querySelector("#name")
         name.textContent = timer.name
+        timerLength = timer.length
+        let h2 = document.querySelector("#timer")
+        h2.textContent = timer.length + ":00"
+        const startbtn = document.querySelector("#start")
+        startbtn.addEventListener('click', function() {
+            let minutes = timerLength
+            let secondsRemaining = minutes * 60
+            let setTimer = setInterval(tick, 1000)   
+            function tick() {
+                            let min = Math.floor(secondsRemaining / 60) 
+                            let sec = secondsRemaining - (min * 60)
+                            if (sec < 10) {
+                                sec = "0" + sec;
+                            }
+                            let timer = min.toString() + ":" + sec;
+                                    h2.textContent = timer
+                            if (secondsRemaining === 0){
+                                alert("Timer is up!")
+                                clearInterval(setTimer);
+                            }
+                            secondsRemaining--;
+                        }
+                        })
     })
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    const timer = document.querySelector("#timer")
-    //let setTimer = setInterval(tick(), 1000)
-    const startbtn = document.querySelector("#start")
-    startbtn.addEventListener('click', function() {
-        startCountDown()
-    })
-    
-});
 
 // let setTimer = setInterval(tick(timer.length), 1000)
 //                         function tick (timerLength) {
