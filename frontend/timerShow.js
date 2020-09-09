1047c265718c3a6b19df9baf835d7320de57abdf
Jay <jaykamas@gmail.com>
2020-09-09 17:20:41 -0400

    Timer has no delay

--- frontend/timerShow.js
+++ frontend/timerShow.js
@@ -10,44 +10,30 @@ fetch(`http://localhost:3000/timers/${id}`)
         let h2 = document.querySelector("#timer")
         h2.textContent = timer.length + ":00"
         const startbtn = document.querySelector("#start")
+        let minutes = timerLength
+        let secondsRemaining = minutes * 60
+        let displayTimer;
         startbtn.addEventListener('click', function() {
-            let minutes = timerLength
-            let secondsRemaining = minutes * 60
-            let setTimer = setInterval(tick, 1000)   
+            displayTimer = setInterval(tick, 1000)
             function tick() {
-                            let min = Math.floor(secondsRemaining / 60) 
-                            let sec = secondsRemaining - (min * 60)
-                            if (sec < 10) {
-                                sec = "0" + sec;
-                            }
-                            let timer = min.toString() + ":" + sec;
-                                    h2.textContent = timer
-                            if (secondsRemaining === 0){
-                                alert("Timer is up!")
-                                clearInterval(setTimer);
-                            }
-                            secondsRemaining--;
-                        }
-                        })
+                let min = Math.floor(secondsRemaining / 60) 
+                let sec = secondsRemaining - (min * 60)
+                if (sec < 10) {
+                    sec = "0" + sec
+                }
+                let timer = min.toString() + ":" + sec
+                h2.textContent = timer
+                if (secondsRemaining === 0){
+                    alert("Timer is up!")
+                    clearInterval(displayTimer)
+                }
+                secondsRemaining--
+            }
+        })
+        const pausebtn = document.querySelector("#pause")
+        pausebtn.addEventListener('click', function() {
+            clearInterval(displayTimer)
+        })
     })
 
 
-// let setTimer = setInterval(tick(timer.length), 1000)
-//                         function tick (timerLength) {
-//                             let minutes = timerLength
-//                             let secondsRemaining = minutes * 60
-//                             let min = Math.floor(secondsRemaining / 60) 
-//                             let sec = secondsRemaining - (min * 60)
-//                             if (sec < 10) {
-//                                 sec = "0" + sec;
-//                             }
-                            
-//                             let timer = min.toString() + ":" + sec;
-                        
-//                             console.log(timer)
-                        
-//                             if (secondsRemaining === 0){
-//                                 console.log("done");
-//                                 clearInterval(setTimer);
-//                             }
-//                             secondsRemaining--;
