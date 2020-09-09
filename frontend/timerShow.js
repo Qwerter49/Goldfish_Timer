aba7e8ea3f7a2706722aaa8bd4114b427c2db047
Jay <jaykamas@gmail.com>
2020-09-09 15:57:48 -0400

    Added Start Button Event Listener

--- frontend/timerShow.js
+++ frontend/timerShow.js
@@ -4,21 +4,19 @@ let id = params.get('id')
 fetch(`http://localhost:3000/timers/${id}`)
     .then(response => response.json())
     .then(timer => {
-        const 
+        const name = document.querySelector("#name")
+        name.textContent = timer.name
     })
 
-
-
-
-
-
-
-
-
-
-
-
-
+window.addEventListener('DOMContentLoaded', (event) => {
+    const timer = document.querySelector("#timer")
+    //let setTimer = setInterval(tick(), 1000)
+    const startbtn = document.querySelector("#start")
+    startbtn.addEventListener('click', function() {
+        startCountDown()
+    })
+    
+});
 
 // let setTimer = setInterval(tick(timer.length), 1000)
 //                         function tick (timerLength) {
